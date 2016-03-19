5ca24ec4498b10ac92fd854bc64a3e605bfc1588
ksami <ksami.ihide@gmail.com>
2016-03-19 12:50:10 +0800

    ending condition and restructure for variable grid size

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 var GRID_ROWS = 4;
 var GRID_COLS = 4;
-var SIZE = 100;
+var GRID_SIZE = 100;  // px
+var TIME_VISIBLE = 750;  // ms
 var COLORS = [
   "#966fd6",
   "#ff6961",
@@ -17,7 +18,8 @@ var COLOR_DONE = "#dfdfdf";
 var COLOR_CURSOR = "#ff0000";
 
 // Populate grid with random paired colors
-var actual = _.chunk(_.shuffle(_.concat(COLORS, COLORS)), GRID_ROWS);
+var availableColors = _.take(COLORS, GRID_ROWS*GRID_COLS/2);
+var actual = _.chunk(_.shuffle(_.concat(availableColors, availableColors)), GRID_ROWS);
 var covers = [];
 var panels = [];
 
@@ -27,7 +29,7 @@ for (var i = 0; i < GRID_ROWS; i++) {
   panels[i] = [];
 
   for (var j = 0; j < GRID_COLS; j++) {
-    var rect = new Rectangle(new Point(j*SIZE, i*SIZE), new Size(SIZE, SIZE));
+    var rect = new Rectangle(new Point(j*GRID_SIZE, i*GRID_SIZE), new Size(GRID_SIZE, GRID_SIZE));
     var panel = new Path.Rectangle(rect);
     panel.strokeColor = COLOR_BORDER;
     panel.fillColor = actual[i][j];
@@ -42,7 +44,7 @@ for (var i = 0; i < GRID_ROWS; i++) {
 }
 
 // Draw cursor
-var cursor = new Path.Rectangle(new Rectangle(new Point(0, 0), new Size(SIZE, SIZE)));
+var cursor = new Path.Rectangle(new Rectangle(new Point(0, 0), new Size(GRID_SIZE, GRID_SIZE)));
 cursor.strokeColor = COLOR_CURSOR;
 cursor.strokeWidth = 2;
 cursor.i = 0;
@@ -55,20 +57,20 @@ function onKeyDown(event){
   
   if(key === "up" && cursor.i > 0){
     cursor.i--;
-    cursor.position -= new Point(0, SIZE);
+    cursor.position -= new Point(0, GRID_SIZE);
   }
   else if(key === "down" && cursor.i < GRID_ROWS-1){
     cursor.i++;
-    cursor.position += new Point(0, SIZE);
+    cursor.position += new Point(0, GRID_SIZE);
   }
 
   if(key === "left" && cursor.j > 0){
     cursor.j--;
-    cursor.position -= new Point(SIZE, 0);
+    cursor.position -= new Point(GRID_SIZE, 0);
   }
   else if(key === "right" && cursor.j < GRID_COLS-1){
     cursor.j++;
-    cursor.position += new Point(SIZE, 0);
+    cursor.position += new Point(GRID_SIZE, 0);
   }
 }
 
@@ -95,13 +97,19 @@ function onKeyUp(event){
       if(actual[prev.i][prev.j] === actual[curr.i][curr.j]){
         panels[prev.i][prev.j].visible = false;
         panels[curr.i][curr.j].visible = false;
+
+        // Game end
+        if(_.every(panels, function(row){return _.every(row, function(p){return !p.visible;});})){
+          console.log("Game end");
+        }
+
       }
       else{
         setTimeout(function(){
           covers[prev.i][prev.j].visible = true;
           covers[curr.i][curr.j].visible = true;
           paper.view.draw();
-        }, 750);
+        }, TIME_VISIBLE);
       }
 
     }
