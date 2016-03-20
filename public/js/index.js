39f21986dcdf3f2a74e6c696e511d72ac6738a6d
ksami <ksami.ihide@gmail.com>
2016-03-20 16:09:54 +0800

    move config vars into different file

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,3 @@
-var GRID_ROWS = 4;
-var GRID_COLS = 4;
-var GRID_SIZE = 100;  // px
-var TIME_VISIBLE = 750;  // ms
-var COLORS = [
-  "#966fd6",
-  "#ff6961",
-  "#03c03c",
-  "#779ecb",
-  "#f49ac2",
-  "#ffb347",
-  "#836953",
-  "#fdfd96"
-];
-var COLOR_BORDER = "#000000";
-var COLOR_COVER = "#cfcfc4";
-var COLOR_BUTTON = "#000000";
-var COLOR_CURSOR = "#ff0000";
-var UI_POS_X = GRID_COLS*GRID_SIZE + 100;  // px
-var UI_SIZE_X = 200;  // px
-
 var gameScore = 0;
 
 var availableColors, actual, covers, panels, openPanels;
